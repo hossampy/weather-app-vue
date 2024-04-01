bb2a2c130939340ebb3cd502597a92a80798daed
hossam <hossameddine.djennad@gmail.com>
2024-04-02 01:47:35 +0200

    api request

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,20 +7,29 @@
       placeholder="Search for a city or state"
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
       />
-      <ul
-        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
-        
+     <ul
+        class="absolute  bg-purple-700 text-white w-full shadow-md py-2 px-1 top-[66px] bg-opacity-50"
+        v-if="mapboxSearchResults"
       >
-        <p class="py-2">
+        <p class="py-2" v-if="searchError">
           Sorry, something went wrong, please try again.
         </p>
         <p
           class="py-2"
-         
+          v-if="!searchError && mapboxSearchResults.length === 0"
         >
           No results match your query, try a different term.
         </p>
-        
+        <template v-else>
+          <li
+            v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2  cursor-pointer capitaliz border-b-2 "
+            @click="previewCity(searchResult)"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
     <div class="flex flex-col gap-4">
@@ -33,6 +42,7 @@
 
 <script setup>
 import axios from 'axios';
+import { useRoute, useRouter } from 'vue-router';
 import { ref } from 'vue';
 const queryTimeout = ref(null);
 const mapboxAPIKey =
@@ -61,6 +71,21 @@ const getSearchResults = () => {
   }, 300);
 };
 
+const router = useRouter ();
+
+const previewCity = (searchResult)=>{
+console.log(searchResult)
+const [city,state] = searchResult.place_name.split(',')
+router.push({
+    name:"cityView",
+     params: { state: state.replaceAll(" ", ""), city: city },
+     query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+})
+}
 
 
 </script>
