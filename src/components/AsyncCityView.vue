939efd493a22ce08b2d0b2a4f2bc699539468871
hossam <hossameddine.djennad@gmail.com>
2024-04-02 03:26:27 +0200

    add some styling to cityperview

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,4 +1,51 @@
 <template>
+    <div class="flex flex-col flex-1 items-center">
+        <div v-if="route.query.preview" class="text-white p-4 bg-weather-secondary w-full text-center">
+                <p>You are currently previewing this city, click the "+"
+        icon to start tracking this city.</p>
+        </div>
+           <!-- Weather Overview -->
+     <div class="flex flex-col items-center text-white py-12">
+        <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+        <p class="text-sm mb-12">
+        {{
+          new Date(weatherData.currentTime).toLocaleDateString(
+            "en-us",
+            {
+              weekday: "short",
+              day: "2-digit",
+              month: "long",
+            }
+          )
+        }}
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString(
+            "en-us",
+            {
+              timeStyle: "short",
+            }
+          )
+        }}
+      </p>
+      <p class="text-8xl mb-8">
+            {{ Math.round(weatherData.current.temp) }} <span class=" absolute text-2xl mr-10 mt-50">°C</span>
+      </p>
+       <p class="capitalize">
+        {{ weatherData.current.weather[0].description }}
+      </p>
+       <img
+        class="w-[100px] h-auto "
+        :src="
+          `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
+        "
+        alt=""
+      />
+
+     </div>
+     <hr class="border-white border-opacity-10 border w-full" />
+
+
+    </div>
   
 </template>
 
@@ -10,7 +57,7 @@ const route = useRoute();
 const getWeatherData = async () => {
   try {
     const weatherData = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=metric`
     );
 
     // cal current date & time
